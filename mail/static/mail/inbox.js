92a39d601674fa75cebf18a60876006694606f54
achrefabdennebi <achref.abdennebi@gmail.com>
2020-10-17 21:26:49 +0200

    feat: add submit behavior

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -8,6 +8,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+  
+  // Submit the form of send mail
+  const form = document.querySelector('#compose-form')
+  // Trigger the submit event
+  form.addEventListener('submit',  (event) => {
+    event.preventDefault();
+    // Get the form data from the event object
+    const formData = new FormData(form)
+    const mailToSend = {
+      recipients: formData.get('recipients'),
+      subject: formData.get('subject'),
+      body: formData.get('body')
+    }
+
+    // Post data of new data
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify(mailToSend)
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Load sent mailbox view
+        load_mailbox('sent');
+    });
+
+    console.log(`Trigger the submit event`);
+  });
 });
 
 function compose_email() {
@@ -22,11 +49,24 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+
+async function getMailboxFromApi(mailbox) {
+  const  emails = await fetch(`/emails/${mailbox}`);
+  const data = await emails.json();
+  return data;
+}
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+
+  // Fetch data of mailbox
+  const fetchedData = getMailboxFromApi(mailbox);
+  fetchedData.then((mailbox)=> {
+    console.log(mailbox);
+  });
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
